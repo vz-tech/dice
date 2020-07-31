2b4acbe56229972089c66d584de97158d814ec9a
vz-tech <59082260+vz-tech@users.noreply.github.com>
2020-07-31 17:03:59 -0400

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,33 +1,3 @@
-// var min = 1;
-// var max = 6;
-
-// function diceSixResult() {
-//     if (min == 1 && max == 6) {
-//       return Math.floor(Math.random()*6 + 1);
-// }
-// }
-//diceSixResult();
-
-// function diceSixResult(max) {
-//   if (typeof(max) === 'number') {
-//     return Math.floor(Math.random()*max + 1);
-//   }
-// else console.log("This is an invalid value.")
-// }
-// diceSixResult(); 
-// Tests if input is a number and enables output with any integer value
-
-// function diceSixResult() {
-//    var max = document.getElementById("myInput").value;
-//    var maxConv = parseInt(max); //Converts string retrieved to int
-//    if (typeof(maxConv) === 'number' && isFinite(max)) {
-//      document.getElementById("output").innerHTML = Math.floor(Math.random()*maxConv + 1);
-//    }
-//    else document.getElementById("output").innerHTML = "This is not a valid number.";
-//  }
-// Preliminary Code
-
-
 function diceSixResult() {
   var max = document.getElementById("myInput").value;
   if (!(/^\d+$/.test(max))) document.getElementById("output").innerHTML = "Please input a valid integer!";
